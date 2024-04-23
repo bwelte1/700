8e62a39bd9bfd9e1394adb5b605e22b4bae64d8c
Benny Welte <benjamin.welte22@gmail.com>
2024-04-24 00:21:52 +1200

    Examples updated

---
+++ Examples/pykep/earth2venus.py
@@ -0,0 +1,50 @@
+# Imports
+import pykep as pk
+import pygmo as pg
+import numpy as np
+
+# Plotting imports
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+# We define the optimization problem
+udp = pk.trajopt.pl2pl_N_impulses(
+    start=pk.planet.jpl_lp('earth'),
+    target=pk.planet.jpl_lp('venus'),
+    N_max=3,
+    tof=[100., 1000.],
+    vinf=[0., 4],
+    phase_free=False,
+    multi_objective=False,
+    t0=[pk.epoch(0), pk.epoch(1000)])
+# All pykep problems in the module trajopt are compatible with pygmo.
+# So we create a pygmo problem from the pykep udp (User Defined Problem)
+prob = pg.problem(udp)
+print(prob)
+
+# Here we define the solution strategy, which in this simple case is to use
+# Covariance Matrix adaptation Evolutionary Strategy (CMA-ES)
+uda = pg.cmaes(gen=1000, force_bounds = True)
+algo = pg.algorithm(uda)
+# Here we activate some degree of screen output (will only show in the terminal)
+algo.set_verbosity(10)
+# We construct a random population of 20 individuals (the initial guess)
+pop = pg.population(prob, size = 20, seed = 123)
+# We solve the problem
+pop = algo.evolve(pop)
+
+
+# Plot our trajectory
+fig = plt.figure(figsize = (16,5))
+ax1 = fig.add_subplot(1, 3, 1, projection='3d')
+ax2 = fig.add_subplot(1, 3, 2, projection='3d')
+ax3 = fig.add_subplot(1, 3, 3, projection='3d')
+ax1 = udp.plot(pop.champion_x, axes = ax1)
+ax2 = udp.plot(pop.champion_x, axes = ax2)
+ax2.view_init(elev=90, azim=0)
+ax3 = udp.plot(pop.champion_x, axes = ax3)
+ax3.view_init(elev=0, azim=0)
+
+# Inspect
+udp.pretty(pop.champion_x)
