2765bd8861b2bab2412aeb71f68abe704b5ef9d3
Benny Welte <benjamin.welte22@gmail.com>
2024-09-29 18:18:51 +1300

    Update to reward, punishment in excessive z axis movement

--- e2m_env.py
+++ e2m_env.py
@@ -183,11 +183,20 @@ class Earth2MarsEnv(gym.Env):
         # print("Mass Change: " + str(reward))
         reward = 0
         reward -= norm(dv) * 20
+        dva = abs(dv)
+        # print(dva)
+        
+        # if dv in z dir is too great
+        if isinstance(dva, list):
+            if dva[-1] > 0.5:
+                reward -= 40
+                reward -= 35*(dva[-1] - 0.5)
 
+        # added punishment for actions > 1
         for ii in range(len(action)):
             if abs(abs(action[ii]) > 1):
                 reward -= 25
-            reward -= 100*max(0, abs(action[ii]) - 1)   # added punishment for actions > 1
+            reward -= 100*max(0, abs(action[ii]) - 1)   
 
         # reward -= 0.05*((max(0, norm(dv) - self.max_thrust)) ** 2)  # added punishment for dv > dv_max
 
@@ -211,6 +220,7 @@ class Earth2MarsEnv(gym.Env):
             if (self.using_reachability == 1):   
                 state0 = np.concatenate((self.r_current, self.v_current))
                 statef = np.concatenate((r_centre, v_centre))
+                # print("Statef: {0}".format(statef))
                 #print("Yaw: " + str(action[0]) + " Pitch: " + str(action[1]) + " Radius: " + str(action[2]))
 
                 #Gets current STM
@@ -335,8 +345,8 @@ class Earth2MarsEnv(gym.Env):
             # print(self.vT)
             # print(lambert_v2)
             dv_equalization = np.subtract(array(self.vT), array(lambert_v2)) # velocity of mars - velocity at final step 
+            # print('dv Eq' + str(norm(dv_equalization)))
             m_next = self.Tsiolkovsky(array(dv_equalization))
-            # print(dv_equalization)
 
             self.m_current = carry_m
             
